79fb2757a43443f31de553fc014b58e233986c8f
Aryan Kumar <aryankumar877@gmail.com>
2024-06-13 14:16:29 +0530

    added middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,17 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(["/signin", "/signup", "/"]);
+
+export default clerkMiddleware((auth, req) => {
+  //user logged in and trying to access public route, redirect to dashboard
+  if (auth().userId && isPublicRoute(req)) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+    //user logged out and trying to access private route, redirect to signin
+  } else if (!auth().userId && !isPublicRoute(req)) {
+    return NextResponse.redirect(new URL("/signin", req.url));
+  }
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
